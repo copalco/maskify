9eac8df42ba4abeaac49093659bc2d4bb62bda00
Piotr Kopalko <copalco@gmail.com>
2019-08-27 14:16:41 +0200

    Add a dummy character stream.

--- src/main/java/maskify/Maskifier.java
+++ src/main/java/maskify/Maskifier.java
@@ -1,11 +1,14 @@
 package maskify;
 
+import java.util.stream.Stream;
+
 public class Maskifier {
     public String maskify(String str) {
         if (str.length() <= 4) {
             return str;
         }
         int charactersToMask = str.length() - 4;
+        Stream<Character> charStream = null;
         return "#".repeat(charactersToMask) + str.substring(charactersToMask);
     }
 }
