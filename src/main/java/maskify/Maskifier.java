7a9fdcd22f11e82a78f9c5e658933d4777ae49a2
Piotr Kopalko <copalco@gmail.com>
2019-08-27 22:20:14 +0200

    Extract logic to domain model

--- src/main/java/maskify/Maskifier.java
+++ src/main/java/maskify/Maskifier.java
@@ -1,14 +1,44 @@
 package maskify;
 
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class Maskifier {
     public String maskify(String str) {
-        if (str.length() <= 4) {
-            return str;
+        return new Masked(str).toString();
+    }
+
+    static class Masked {
+
+        private final String text;
+
+        public Masked(String text) {
+            this.text = text;
         }
-        int charactersToMask = str.length() - 4;
-        Stream<Character> charStream = str.chars().mapToObj(c -> (char) c);
-        return "#".repeat(charactersToMask) + str.substring(charactersToMask);
+
+        public String characterAt(int index) {
+            return shouldMaskAt(index) ? "#" : unmaskedCharacterAt(index);
+        }
+
+        private boolean shouldMaskAt(int index) {
+            return index < firstUnmaskedIndex();
+        }
+
+        private int firstUnmaskedIndex() {
+            return length() - 4;
+        }
+
+        private String unmaskedCharacterAt(int index) {
+            return String.valueOf(text.charAt(index));
+        }
+
+        @Override
+        public String toString() {
+            return IntStream.range(0, length()).mapToObj(i -> characterAt(i)).reduce("", String::concat);
+        }
+
+        private int length() {
+            return text.length();
+        }
+
     }
 }
